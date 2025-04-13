b91034e1a020c69b221a14a6795ea142682b8e89
theeko26 <eko26@nxt.ru>
2025-04-14 01:44:15 +0500

    Dev : dict 'aviation_numbers' → main.py
    (cherry picked from commit 274d6e724e7e49b851f2165dac14656fdf75d7f3)

--- src/main.py
+++ src/main.py
@@ -27,3 +27,15 @@ aviation_alphabet = {
     'Z': 'Zulu'
 }
 
+aviation_numbers = {
+    '1': 'One',
+    '2': 'Two',
+    '3': 'Three',
+    '4': 'Four',
+    '5': 'Five',
+    '6': 'Six',
+    '7': 'Seven',
+    '8': 'Eight',
+    '9': 'Niner',
+    '0': 'Zero'
+}
